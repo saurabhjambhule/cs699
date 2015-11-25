651d50bbd1c6ce3af0e917172fd95ce6cc722c40
rexruler <rexruler001@gmail.com>
2015-11-25 21:12:54 +0530

    newSql

--- proj_slot (2).sql
+++ proj_slot (2).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2015 at 03:29 PM
+-- Generation Time: Nov 25, 2015 at 04:41 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `proj_slot` (
+  `id` varchar(20) NOT NULL,
   `time` varchar(10) NOT NULL,
   `date` varchar(10) NOT NULL,
   `duration` varchar(6) NOT NULL,
@@ -34,16 +35,16 @@ CREATE TABLE IF NOT EXISTS `proj_slot` (
   `init_id` varchar(10) DEFAULT NULL,
   `purpose` varchar(100) NOT NULL,
   `approve` varchar(10) NOT NULL,
-  PRIMARY KEY (`time`,`date`)
+  PRIMARY KEY (`time`,`date`),
+  KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `proj_slot`
 --
 
-INSERT INTO `proj_slot` (`time`, `date`, `duration`, `init_name`, `init_id`, `purpose`, `approve`) VALUES
-('11:30 ', '9-11-2015', '30 Min', 'jgfyfuyj', '24324234', 'AAASAAAA ', 'pending'),
-('6:30 A', '18-11-2015', '60 Min', 'asfasf', 'adada', 'jaj ', 'pending');
+INSERT INTO `proj_slot` (`id`, `time`, `date`, `duration`, `init_name`, `init_id`, `purpose`, `approve`) VALUES
+('13-11-201511:30 AM', '11:30 AM', '13-11-2015', '30 Min', 'jgfyfuyj', '24324234', 'AAASAAAA ', 'pending');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
